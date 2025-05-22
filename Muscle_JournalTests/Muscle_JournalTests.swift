e28645bfa2d05c3bb794012966670b5e83eae72e
Mohsin <mohsin.husaynn@gmail.com>
2025-05-22 11:31:45 +0400

    Initial Commit

---
+++ Muscle_JournalTests/Muscle_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Muscle_JournalTests.swift
+//  Muscle_JournalTests
+//
+//  Created by Abbas on 22/05/2025.
+//
+
+import Testing
+@testable import Muscle_Journal
+
+struct Muscle_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
